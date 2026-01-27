2760e31311b7cde4df0021181301e88542bae8f3
BaekDongyun2 <bdy0010317@gmail.com>
2026-01-27 14:29:01 +0900

    arduino5 & javascript1

---
+++ 16_WIFI_02/16_WIFI_02.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial myESP(2, 3); // D2 = RX(ESP TX), D3 = TX(ESP RX)
+
+// ESP 모듈 속도가 9600으로 바뀌었는지 확인
+void setup() {
+  Serial.begin(9600);
+  myESP.begin(9600);
+  Serial.println("================TEST================");
+}
+
+void loop() {
+  if(myESP.available()) {
+    Serial.write(myESP.read());
+  }
+  if(Serial.available()) {
+   myESP.write(Serial.read());
+  }
+}
+
+
+/*
+AT+CWJAP="spreatics_gusan_cctv","spreatics*"
+*/
